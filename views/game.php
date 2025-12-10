255b6bb8130a54ebf470af1fbe73922a917b00bd
Cobat <nathancobat1@gmail.com>
2025-12-10 10:16:54 +0100

    ajuste tabbleau

--- views/game.php
+++ views/game.php
@@ -1,6 +1,51 @@
+<?php
+  $tableau = [];
+
+  for ($i = 0; $i < 11; $i++) {
+      for ($j = 0; $j < 11; $j++) {
+          if ($i == 0 && $j == 0) {
+              $tableau[$i][$j] = ""; 
+          } elseif ($i == 0) {
+            
+              $tableau[$i][$j] = chr(ord('A') + $j - 1);
+          } elseif ($j == 0) {
+              
+              $tableau[$i][$j] = $i;
+          } else {
+          
+              $tableau[$i][$j] = '<form method="post"><button>O</button></form>';
+          }
+      }
+  }
+
+
+  echo "<table border='1' cellpadding='5' cellspacing='0'>";
+  for ($i = 0; $i < 11; $i++) {
+      echo "<tr>";
+      for ($j = 0; $j < 11; $j++) {
+          echo "<td>" . $tableau[$j][$i] . "</td>";
+      }
+      echo "</tr>";
+  }
+  echo "</table>";
+
+
+  $batiments = [
+      ["id" => 4, "nom" => "Porte-avion", "taille" => 5],
+      ["id" => 3, "nom" => "Croiseur", "taille" => 4],
+      ["id" => 2, "nom" => "Sous-marin", "taille" => 3],
+      ["id" => 2, "nom" => "Sous-marin", "taille" => 3],
+      ["id" => 1, "nom" => "Torpilleur", "taille" => 2]
+  ];
+
+  header('refresh:5');
+?>
+
+
+<!--
 <!DOCTYPE html>
 <html>
-  <head>
+  <head>  
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Game</title>
@@ -123,6 +168,7 @@
         <div class="col">0</div>
       </div>
     </div>
+-->    
     <form method="post" action="../scripts/reset_total.php">
       <button type="submit" name="reset_total">
         ❌ Fin de partie (RESET)
