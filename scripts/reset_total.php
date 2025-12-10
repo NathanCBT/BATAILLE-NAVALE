24042ea91eb62e8a6aed10ab50ebf429d5b3efeb
Tp411 <thomas.pisanne@coda-student.school>
2025-12-10 15:30:41 +0100

    modif

--- scripts/reset_total.php
+++ scripts/reset_total.php
@@ -1,7 +1,10 @@
 <?php
   if (isset($_POST["reset_total"])) {
     include('./destory_session.php');
-
+    include('./init_boats.php');
+    
     header("Location: ../index.php");
     exit;
+   
+
   }
