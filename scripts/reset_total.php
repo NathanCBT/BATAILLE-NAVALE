91f47b8df6315ac7548e9357e4ae2439d87bebaf
Tp411 <thomas.pisanne@coda-student.school>
2025-12-11 09:43:10 +0100

    test grid et update sql

--- scripts/reset_total.php
+++ scripts/reset_total.php
@@ -1,7 +1,7 @@
 <?php
   if (isset($_POST["reset_total"])) {
     include('./destory_session.php');
-    include('./init_boats.php');
+    include('./init_boat.php');
     
     header("Location: ../index.php");
     exit;
